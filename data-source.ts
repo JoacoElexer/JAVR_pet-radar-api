0dc15382b9e73528594306e29535a4ec18458b35
Joaco_Vrz <joaco0017@hotmail.com>
2026-06-23 01:33:41 -0600

    db migrationsfix

--- data-source.ts
+++ data-source.ts
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'admin',
-    password: 'root',
-    database: 'petradar_db',
-    entities: ['src/**/*.entity.ts'],
-    migrations: ['src/migrations/*.ts'],
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASS || 'root',
+    database: process.env.DB_NAME || 'petradar_db',
+    entities: ['dist/src/**/*.entity.js'],
+    migrations: ['dist/src/migrations/*.js'],
 });
--- dockerfile
+++ dockerfile
@@ -9,8 +9,9 @@ RUN npm prune --omit=dev
 
 FROM node:22-alpine AS production
 WORKDIR /app
+COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/node_modules ./node_modules
 EXPOSE 3000
 ENV NODE_ENV=production
-CMD ["node", "--max-old-space-size=350", "dist/src/main.js"]
+CMD ["sh", "-c", "node --max-old-space-size=350 node_modules/typeorm/cli.js migration:run -d dist/data-source.js && node --max-old-space-size=350 dist/src/main.js"]
